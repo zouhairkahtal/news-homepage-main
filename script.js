301d9912f6e88ec5577055ead62df07a87c11c91
zouhair <mizouhair4@gmail.com>
2024-07-04 19:28:37 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    const Sidebar = document.querySelector(".sidebar");
+    Sidebar.style.display = 'flex'
+  }
+  
+  function closeSidebar() {
+      const Sidebar = document.querySelector(".sidebar");
+      Sidebar.style.display = 'none'
+    }
+    
